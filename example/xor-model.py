92738352d95b3aaac3e6735e65160f9b8a4f1ecd
zarc69 <zarc69@gmail.com>
2022-08-19 00:30:57 +1000

    Binary model

--- example/xor-model.py
+++ example/xor-model.py
@@ -5,40 +5,45 @@ import numpy as np
 
 from keras import layers
 
-# column_names = ['i1', 'i2', 'o1']
-#
-# raw_dataset = pd.read_csv('data/data.csv', names=column_names,
-#                           na_values='?', sep=',', skipinitialspace=True)
-#
-# dataset = raw_dataset.copy()
-# dataset.tail()
-train_data = tf.constant([
+x_train = tf.cast([
     [0, 0],
     [0, 1],
     [1, 0],
     [1, 1]
-])
-print(train_data)
-validation_data = tf.constant([
-    1, 0, 0, 1
-])
-inputs = keras.Input(shape=(4, 2), dtype='int32', name='input_layer_1')
+], tf.float32)
+y_train = tf.cast([0, 1, 1, 0], tf.float32)
+x_test = tf.cast([
+    [0, 0],
+    [1, 1],
+    [0, 1],
+    [1, 0]
+], tf.float32)
+y_test = tf.cast([0, 0, 1, 1], tf.float32)
 
-dense = layers.Dense(units=1, name="output_layer_1")
-outputs = dense(inputs)
-
-model = keras.Model(inputs, outputs)
+model = keras.Sequential([
+    keras.layers.Input(shape=(2,)),
+    keras.layers.Dense(units=2, activation='relu'),
+    keras.layers.Dense(units=1, activation='sigmoid')
+])
 model.summary()
 
 model.compile(
-    loss=keras.losses.SparseCategoricalCrossentropy(from_logits=True),
-    optimizer=keras.optimizers.Adam(),
+    loss=keras.losses.BinaryCrossentropy(from_logits=False),
+    optimizer='adam',
     metrics=["accuracy"]
 )
 
-history = model.fit(train_data, validation_data, batch_size=4, epochs=10)
-test_data = train_data
+history = model.fit(
+    x=x_train, y=y_train,
+    epochs=1000,
+    steps_per_epoch=1,
+    validation_data=(x_test, y_test),
+    validation_steps=1
+)
 
-model.predict(test_data, verbose=2)
+predictions = model.predict(x_test)
+
+print(predictions)
+
 # keras.utils.plot_model(model, "my_first_model.png")
 # keras.utils.plot_model(model, "my_first_model_with_shape_info.png", show_shapes=True)
