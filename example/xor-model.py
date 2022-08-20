cfa1ee41f5bdd21cc84bab616736b0d831d75d59
zarc69 <zarc69@gmail.com>
2022-08-20 13:46:52 +1000

    Binary model

--- example/xor-model.py
+++ example/xor-model.py
@@ -6,19 +6,61 @@ import numpy as np
 from keras import layers
 
 x_train = tf.cast([
-    [0, 0],
-    [0, 1],
-    [1, 0],
-    [1, 1]
+    [0.22, 0.22],
+    [0.22, 1.22],
+    [1.22, 0.22],
+    [1.22, 1.22],
+    [0.1, 0.1],
+    [0.1, 1.1],
+    [1.1, 0.1],
+    [1.1, 1.1],
+    [0.75, 0.75],
+    [0.75, 1.75],
+    [1.75, 0.75],
+    [1.75, 1.75],
+    [0.78, 0.78],
+    [0.78, 1.78],
+    [1.78, 0.78],
+    [1.78, 1.78],
+    [0.7, 0.7],
+    [0.7, 1.7],
+    [1.7, 0.7],
+    [1.7, 1.7]
 ], tf.float32)
-y_train = tf.cast([0, 1, 1, 0], tf.float32)
+x_train = 1 / x_train
+y_train = tf.cast([0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0], tf.float32)
+x_validation = tf.cast([
+    [0.6, 0.6],
+    [1.6, 1.6],
+    [0.6, 1.6],
+    [1.6, 0.6],
+    [0.9, 0.9],
+    [1.9, 1.9],
+    [0.9, 1.9],
+    [1.9, 0.9],
+    [0.91, 0.91],
+    [1.91, 1.91],
+    [0.91, 1.91],
+    [1.91, 0.91]
+], tf.float32)
+x_validation = 1 / x_validation
+y_validation = tf.cast([0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1], tf.float32)
 x_test = tf.cast([
-    [0, 0],
-    [1, 1],
-    [0, 1],
-    [1, 0]
+    [0.5, 0.5],
+    [1.5, 1.5],
+    [0.5, 1.5],
+    [1.5, 0.5],
+    [0.2, 0.2],
+    [1.2, 1.2],
+    [0.2, 1.2],
+    [1.2, 0.2],
+    [0.31, 0.31],
+    [1.31, 1.31],
+    [0.31, 1.31],
+    [1.31, 0.31]
 ], tf.float32)
-y_test = tf.cast([0, 0, 1, 1], tf.float32)
+x_test = 1 / x_test
+y_test = tf.cast([0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1], tf.float32)
 
 model = keras.Sequential([
     keras.layers.Input(shape=(2,)),
@@ -28,21 +70,23 @@ model = keras.Sequential([
 model.summary()
 
 model.compile(
-    loss=keras.losses.BinaryCrossentropy(from_logits=False),
+    loss='binary_crossentropy',
     optimizer='adam',
     metrics=["accuracy"]
 )
 
 history = model.fit(
     x=x_train, y=y_train,
-    epochs=1000,
+    epochs=48,
+    batch_size=4,
     steps_per_epoch=1,
-    validation_data=(x_test, y_test),
+    validation_data=(x_validation, y_validation),
     validation_steps=1
 )
+loss, accuracy = model.evaluate(x_test, y_test)
+print("Accuracy: ", accuracy)
 
-predictions = model.predict(x_test)
-
+predictions = model.predict(x_test, batch_size=4)
 print(predictions)
 
 # keras.utils.plot_model(model, "my_first_model.png")
